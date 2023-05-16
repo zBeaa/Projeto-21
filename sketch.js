85c5ea4aca5d23df81bb8e31f8b3d68771931041
zBeaa <129691383+zBeaa@users.noreply.github.com>
2023-05-16 16:25:53 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,134 @@
+  
+var towerImg, tower;
+var doorImg, door, doorsGroup;
+var climberImg, climber, climbersGroup;
+var ghost, ghostImg;
+var invisibleBlockGroup, invisibleBlock;
+var gameState = "play";
+
+
+function preload(){
+  towerImg = loadImage("tower.png");
+  doorImg = loadImage("door.png");
+  climberImg = loadImage("climber.png");
+  ghostImg = loadImage("ghost-standing.png");
+  spookySound = loadSound("spooky.wav");
+}
+
+function setup() {
+  createCanvas(600,600);
+  spookySound.loop();
+  tower = createSprite(300,300);
+  tower.addImage("tower",towerImg);
+  tower.velocityY = 1;
+  
+  doorsGroup = new Group();
+  climbersGroup = new Group();
+  invisibleBlockGroup = new Group();
+  
+  ghost = createSprite(200,200,50,50);
+  ghost.scale = 0.3;
+  ghost.addImage("ghost", ghostImg);
+}
+
+
+function draw() {
+  background(255);
+ if(tower.y > 0){
+      tower.y = 300;
+    } 
+  
+  if (gameState === "play") {
+    
+    if(keyDown("LEFT_ARRROW")){
+        ghost.x = ghost.x - 3;
+
+      // escreva o código para mover para a esquerda quando a tecla para a esquerda for pressionada
+    }
+    if(keyDown("RIGHT_ARROW")){
+  
+         ghost.x = ghost.x + 3;
+
+      // escreva o código para mover para a direita quando a tecla para a direita for pressionada
+      
+    }
+    if(keyDown("UP_ARROW")){
+  
+         ghost.velocityY = -10;
+
+      // escreva o código para mover para cima quando a tecla espaço for pressionada
+      
+    }
+  
+  ghost.velocityY = ghost.velocityY + 0.8;
+
+      //escreva uma condição para a torre de rolagem infinita
+      if(tower.y > 400 ){
+        tower.y = 300;
+      }
+      spawnDoors();
+
+ 
+//escreva um código para fazer invisibleBlockGroup (grupo de bloco invisível) colidir com o fantasma, destruir o fantasma e mudar o estado do jogo para end.
+     if(climbersGroup.isTouching(ghost)){
+      ghost.velocityY = 0;
+    }
+    if(invisibleBlockGroup.isTouching(ghost) || ghost.y > 600){
+      ghost.destroy;
+      gameState = "END";
+    }
+    
+  
+  drawSprites();
+      }
+}
+  if (gameState === "end"){
+    stroke("yellow");
+    fill("yellow");
+    textSize(30);
+    text("Game Over", 230,250);
+  }
+
+
+function spawnDoors()
+ {
+  //escreva o código aqui para gerar as nuvens
+  if (frameCount % 240 === 0) {
+    var door = createSprite(200, -50);
+    var climber = createSprite(200,10);
+    var invisibleBlock = createSprite(200,15);
+    invisibleBlock.width = climber.width;
+    invisibleBlock.height = 2;
+    addImage(doorImg);
+    //adicione a função aleatória
+    door.x = Math.round(random (120, 400));
+    door.addImage(doorImg);
+    climber.x = door.x;
+    invisibleBlock.x = door.x;
+    climber.addImage(climberImg);
+    
+    door.velocityY = 1;
+    climber.velocityY = 1;
+    invisibleBlock.velocityY = 1;
+
+    //mude a profundidade do fantasma e da porta
+    ghost.depth = door.depht;
+    ghost.depth = ghost.depth + 1;
+     
+ghost.depth = door.depth;
+    ghost.depth =1;
+    
+    //atribuir tempo de vida para a porta, escalador e bloco invisível
+
+    door.lifetime = 800;
+    climber.lifetime = 800;
+    invisibleBlock.lifetime = 800;
+    //adicione cada obstáculo ao grupo obstaclesGroup.add(obstacle); aqui os obstáculos são as portas, o escalador e o bloco invisível
+    obstaclesGroup.add(doorsGroup, invisibleBlockGroup, climbersGroup);
+    doorsGroup.add();
+    invisibleBlock.debug = true;
+    climbersGroup.add();
+    invisibleBlockGroup.add();
+  }
+}
+
